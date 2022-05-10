40794a78411c7d160796461109c56eaedd353d6a
MarlinShenoda <marlinadel2000@gmail.com>
2022-05-10 17:14:38 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv your namn");
+            var namn=Console.ReadLine();
+            Console.WriteLine("skriv your lön");
+            int lön = int.Parse(Console.ReadLine());
+            Console.WriteLine(namn+"ha lön"+lön);
+        }
+    }
+}
